2a2121fd33ab16d860927d0166e0cf2fd270dfd5
noah172 <jsebastian.garcia1995@gmail.com>
2020-11-30 15:58:21 -0500

    fixed: adding slash in the dir

--- 0x00-csharp-hello_world/1-new_project.sh
+++ 0x00-csharp-hello_world/1-new_project.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 #creatin and inizialiting the project
-dotnet new console -o ./1-new_project
-dotnet build ./1-new_project
+dotnet new console -o ./1-new_project/
+dotnet build ./1-new_project/

b0a580533856721fa08a58ee224d6c9b5340442f
noah172 <jsebastian.garcia1995@gmail.com>
2020-12-01 15:08:46 -0500

    task 6

---
+++ 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _6_print_comb2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0, j = 0;
+            for (; i <= 9; j++)
+            {
+                if (j > 9)
+                {
+                    i += 1;
+                    j = i + 1;
+                }
+                if (i == 8)
+                {
+                    Console.Write("{0:D}{1:D}\n", i, j);
+                    break;
+                }
+                Console.Write("{0:D}{1:D}, ", i, j);
+            }
+        }
+    }
+}

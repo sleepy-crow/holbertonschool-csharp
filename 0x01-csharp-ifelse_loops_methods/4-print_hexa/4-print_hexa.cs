c68a81bb904406011e5c76160876a23180474ccd
noah172 <jsebastian.garcia1995@gmail.com>
2020-12-01 12:22:05 -0500

    task 4: convert desimal to hex

---
+++ 0x01-csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _4_print_hexa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            for (i = 0;i < 99; i++){
+                string hex = i.ToString("X");
+                Console.Write("{0:D} = {1}\n", i, hex);
+            }
+        }
+    }
+}

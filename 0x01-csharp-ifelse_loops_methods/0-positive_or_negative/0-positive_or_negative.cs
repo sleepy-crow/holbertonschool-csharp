ee59e4d13e5a6fe27204ebf59221705efa356cb3
noah172 <jsebastian.garcia1995@gmail.com>
2020-12-01 10:05:59 -0500

    task 0: positive and negative conditional

---
+++ 0x01-csharp-ifelse_loops_methods/0-positive_or_negative/0-positive_or_negative.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random rndm = new Random();
+        int number = rndm.Next(-10, 10);
+        if (number > 0)
+            Console.WriteLine($"{number:D} is positive");
+        else if (number < 0)
+            Console.WriteLine($"{number:D} is negative");
+        else
+            Console.WriteLine($"{number:D} is zero");
+    }
+}

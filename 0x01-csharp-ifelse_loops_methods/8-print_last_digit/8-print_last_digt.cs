fa687f1bcb743cb43bab117874fdfd5259b65225
noah172 <jsebastian.garcia1995@gmail.com>
2020-12-01 17:09:39 -0500

    task 8

---
+++ 0x01-csharp-ifelse_loops_methods/8-print_last_digit/8-print_last_digt.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+class Number
+{
+    public static int PrintLastDigit(int number)
+    {
+        int i;
+        if (number > 0)
+            i = number % 10;
+        else
+            i = (number % 10) * -1;
+
+        Console.Write("{0:D}", i);
+        return(i);
+        
+    }
+}

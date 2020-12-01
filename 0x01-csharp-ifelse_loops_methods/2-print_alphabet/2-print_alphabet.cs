653f2b6484fad1c24ec690b31aea6adacd0bba86
noah172 <jsebastian.garcia1995@gmail.com>
2020-12-01 12:05:44 -0500

    fixed print all the alphabet

--- 0x01-csharp-ifelse_loops_methods/2-print_alphabet/2-print_alphabet.cs
+++ 0x01-csharp-ifelse_loops_methods/2-print_alphabet/2-print_alphabet.cs
@@ -8,10 +8,7 @@ namespace _2_print_alphabet
         {
             int i;
             for (i = 97; i <= 122; i++)
-            {
-                if (i == 101 || i == 113)
-                    i += 1;
-                
+            {            
                 char c = Convert.ToChar(i);
                 Console.Write("{0}", c);
             }

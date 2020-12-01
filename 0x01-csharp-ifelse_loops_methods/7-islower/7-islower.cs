c005f8fd8b51d595d5dea8d504885913decaa9d6
noah172 <jsebastian.garcia1995@gmail.com>
2020-12-01 16:16:30 -0500

    task 7:

---
+++ 0x01-csharp-ifelse_loops_methods/7-islower/7-islower.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _7_islower
+{
+    class Character
+    {
+        public static bool IsLower(char c)
+        {
+            bool ans = false;
+            for (char cc = 'a'; cc <= 'z'; cc++)
+            {
+                if (cc == c)
+                    ans = true;
+            }
+            
+            return(ans);
+        }
+    }
+}

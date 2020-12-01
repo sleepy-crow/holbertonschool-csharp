b1ee5cfde0f9f811f165eeff82ef35e28f3a37d4
noah172 <jsebastian.garcia1995@gmail.com>
2020-12-01 13:06:20 -0500

    fixed: how the hex is showed

--- 0x01-csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
+++ 0x01-csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -9,7 +9,7 @@ namespace _4_print_hexa
             int i;
             for (i = 0;i < 99; i++){
                 string hex = i.ToString("X");
-                Console.Write("{0:D} = {1}\n", i, hex);
+                Console.Write("{0:D} = 0x{1}\n", i, hex);
             }
         }
     }

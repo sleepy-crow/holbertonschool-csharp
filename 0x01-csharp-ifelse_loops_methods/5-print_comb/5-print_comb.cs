8ff7630abeea4e518dce181c73022d8054d2beac
noah172 <jsebastian.garcia1995@gmail.com>
2020-12-01 13:44:50 -0500

    task 5: print numbers form 0 to 99

---
+++ 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _5_print_comb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            for (i = 0; i <= 99; i++)
+            {
+                if (i < 99)
+                    Console.Write("{0:D2}, ", i);
+                else
+                     Console.Write("{0:D2}\n", i);
+            }
+        }
+    }
+}

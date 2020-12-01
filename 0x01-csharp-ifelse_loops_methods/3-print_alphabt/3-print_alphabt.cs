c08bf8be2b716b75332defd0c4679d6b0145f6d5
noah172 <jsebastian.garcia1995@gmail.com>
2020-12-01 12:09:11 -0500

    task 3: print alphabet except 'e' and 'q'

---
+++ 0x01-csharp-ifelse_loops_methods/3-print_alphabt/3-print_alphabt.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _3_print_alphabt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            for (i = 97; i <= 122; i++)
+            {
+                if (i == 101 || i == 113)
+                    i += 1;            
+                char c = Convert.ToChar(i);
+                Console.Write("{0}", c);
+            }
+        }
+    }
+}

1e4dac20faa1a76a296e386693502352ae92261a
noah172 <jsebastian.garcia1995@gmail.com>
2020-12-01 15:29:23 -0500

    task 4

--- 0x01-csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
+++ 0x01-csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -7,9 +7,8 @@ namespace _4_print_hexa
         static void Main(string[] args)
         {
             int i;
-            for (i = 0;i < 99; i++){
-                string hex = i.ToString("X");
-                Console.Write("{0:D} = 0x{1}\n", i, hex);
+            for (i = 0;i <= 98; i++){
+                Console.Write("{0:D} = 0x{0:X}\n", i);
             }
         }
     }

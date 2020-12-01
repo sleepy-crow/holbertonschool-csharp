61f28105ec0899fae61133c671690cd3b7aa5f7e
noah172 <jsebastian.garcia1995@gmail.com>
2020-12-01 17:43:35 -0500

    task 12

---
+++ 0x01-csharp-ifelse_loops_methods/12-fizzbuzz/12-fizzbuzz.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _12_fizzbuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 15 == 0)
+                    Console.Write("FizzBuzz");
+                else if (i % 5 == 0)
+                    Console.Write("Buzz");
+                else if (i % 3 == 0)
+                    Console.Write("Fizz");
+                else
+                    Console.Write($"{i}");
+
+                Console.Write((i != 100) ? " " : "\n");
+            }
+        }
+    }
+}

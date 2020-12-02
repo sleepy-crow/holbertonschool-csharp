a820bfcd9817b49fdc246cbb67c0d802734ba88b
noah172 <jsebastian.garcia1995@gmail.com>
2020-12-02 12:41:46 -0500

    task 1: First commit

---
+++ 0x02-csharp-arrays_lists_dictionaries/1-element_at/1-element_at.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+class Array
+{
+    public static int elementAt(int[] array, int index)
+    {
+        int size = array.Length;
+        if (index > size || index < 0)
+        {
+            Console.WriteLine("Index out of range");
+            return -1;
+        }
+        else
+        {
+            int i = array[index];
+            return i;
+        }
+    }
+}

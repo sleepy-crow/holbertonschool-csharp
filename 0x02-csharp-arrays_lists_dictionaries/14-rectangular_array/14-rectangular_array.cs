b2789ecaad2f34d77babf75dee6f74ca148cbb62
noah172 <jsebastian.garcia1995@gmail.com>
2020-12-02 17:34:06 -0500

    task 14

---
+++ 0x02-csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _14_rectangular_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 5;
+            int j = 5;
+
+            int[,] rabbitHole = new int[i, j];
+
+            rabbitHole[2, 2] = 1;
+            for (int w = 0; w < i; w++)
+            {
+                for (int h = 0; h < j; h++)
+                {
+                    Console.Write(rabbitHole[w, h]);
+                    if (h < j - 1)
+                        Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}

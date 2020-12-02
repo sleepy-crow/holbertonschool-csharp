3cb69edcdd26aa45ae053f67c3752a8f21a1fcff
noah172 <jsebastian.garcia1995@gmail.com>
2020-12-02 15:57:50 -0500

    task 8

---
+++ 0x02-csharp-arrays_lists_dictionaries/8-number_keys/8-number_keys.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class Dictionary
+{
+    public static int NumberOfKeys(Dictionary<string, string> myDict)
+    {
+        int c = 0;
+        foreach (string element in myDict.Keys)
+            c++;
+        return (c);
+    }
+}

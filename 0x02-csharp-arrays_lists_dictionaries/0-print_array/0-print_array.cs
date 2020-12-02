88f9d893d9eaecc57b080a568ce52ff8e120a14f
noah172 <jsebastian.garcia1995@gmail.com>
2020-12-02 12:18:51 -0500

    task 0: First commmit

---
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+
+class Array
+{
+    public static int[] CreatePrint(int size)
+    {
+        if (size > 0)
+        {
+            int i, j;
+            int[] array = new int[size];
+            for (i = 0; i < size; i++)
+            {
+                array[i] = i;
+            }
+            for (j = 0; j < size; j++)
+            {
+                if (j != size - 1)
+                    Console.Write(array[j] + " ");
+                else
+                    Console.Write(array[j] + "\n");
+            }
+            return array;
+        }
+        else if (size == 0)
+        {
+            int[] array = new int[size];
+            Console.WriteLine("");
+            return array;
+        }
+        else
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+    }
+}

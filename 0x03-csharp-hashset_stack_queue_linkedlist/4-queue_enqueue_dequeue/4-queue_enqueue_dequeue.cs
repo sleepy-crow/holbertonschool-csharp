479b97fd7fbbfbb6582fa9f3cd027911c1aeb762
noah172 <jsebastian.garcia1995@gmail.com>
2020-12-03 16:27:53 -0500

    task 4

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MyQueue
+{
+    public static Queue<string> Info(Queue<string> aQueue, string newItem, string search)
+    {
+        Console.WriteLine("Number of items: {0}", aQueue.Count);
+
+        if (aQueue.Count == 0)
+            Console.WriteLine("Queue is empty");
+        else
+            Console.WriteLine("First item: {0}", aQueue.Peek());
+        
+        if (newItem != null)
+            aQueue.Enqueue(newItem);
+        
+        bool contains = aQueue.Contains(search);
+        
+        Console.WriteLine("Queue contains \"{0}\": {1}", search, contains);
+
+        Queue<string> neoQueue = new Queue<string>(aQueue);
+
+        if (aQueue.Contains(search))
+            foreach (var item in neoQueue)
+            {
+                if (aQueue.Dequeue() == search)
+                    break;
+            }
+
+
+        return aQueue;
+    }
+}

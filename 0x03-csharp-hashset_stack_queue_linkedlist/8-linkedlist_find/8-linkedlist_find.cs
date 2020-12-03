6e35ba36f7ea1f9618ff94a5995061a929c7cdcb
noah172 <jsebastian.garcia1995@gmail.com>
2020-12-03 18:16:31 -0500

    task 8

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/8-linkedlist_find/8-linkedlist_find.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+    class LList
+    {
+        public static int FindNode(LinkedList<int> myLList, int value)
+        {
+            int i = 0;
+            foreach (int e in myLList)
+            {
+                if (e == value)
+                    return i;
+                i++;
+            }
+            return -1;
+        }
+    }

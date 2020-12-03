921989c202365dfc3acdc00e9876e460b363f7d4
noah172 <jsebastian.garcia1995@gmail.com>
2020-12-03 18:44:47 -0500

    task 11

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/11-linkedlist_sum/11-linkedlist_sum.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int Sum(LinkedList<int> myLList)
+    {
+        int i = 0;
+        foreach (int x in myLList)
+            i += x;
+        
+        return i;
+    }
+}

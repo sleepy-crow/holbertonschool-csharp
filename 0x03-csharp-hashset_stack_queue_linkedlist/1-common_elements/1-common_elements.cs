3fb32813d666da28e2ee7d3c13c0395426bb95ab
noah172 <jsebastian.garcia1995@gmail.com>
2020-12-03 13:30:35 -0500

    task 1

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class List
+{
+    public static List<int> CommonElements(List<int> list1, List<int> list2)
+    {
+        List<int> glitch = new List<int>();
+
+        foreach (int i in list1)
+        {
+            foreach (int j in list2)
+            {
+                if (i == j)
+                {
+                    glitch.Add(i);
+                }
+                else
+                {
+                    continue;
+                }
+            }
+        }
+        return glitch;
+    }
+}

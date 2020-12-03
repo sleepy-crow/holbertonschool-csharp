34a3e37fea050035ed30b625cb5a699343502967
noah172 <jsebastian.garcia1995@gmail.com>
2020-12-03 18:31:20 -0500

    task 9

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/9-linkedlist_pop/9-linkedlist_pop.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int Pop(LinkedList<int> myLList)
+    {
+        if (myLList.Count == 0)
+            return (0);
+        
+        int i = myLList.First.Value;
+        myLList.RemoveFirst();
+        return (i);
+    }
+}

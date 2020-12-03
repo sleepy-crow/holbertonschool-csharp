6f4060058ae54509560ff41dd1505df89127a82c
noah172 <jsebastian.garcia1995@gmail.com>
2020-12-03 14:02:26 -0500

    task 2

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class List
+{
+    public static List<int> DifferentElements(List<int> list1, List<int> list2)
+    {
+        List<int> notGlitched = new List<int>();
+
+        foreach (int i in list1)
+        {
+            if (!list2.Contains(i))
+                notGlitched.Add(i);
+        }
+
+        foreach (int j in list2)
+        {
+            if (!list1.Contains(j))
+                notGlitched.Add(j);
+        }
+
+        notGlitched.Sort();
+
+        return notGlitched;
+    }
+}

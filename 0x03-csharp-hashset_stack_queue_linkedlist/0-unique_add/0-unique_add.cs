ce70246856d8d5e9e6aad6bd27722113fcc9e3c1
noah172 <jsebastian.garcia1995@gmail.com>
2020-12-03 12:06:00 -0500

    task 0

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+    class List
+    {
+        public static int Sum(List<int> myList)
+        {
+            int neo =  0, x = 0, y = 0;
+            foreach (int e in myList)
+            {
+                y += 1;
+                if (y > 1)
+                {
+                    if (e == x)
+                        continue;
+                    else
+                        neo += e;
+                }
+                else
+                {
+                    neo += e;
+                }
+                x = e;
+            }
+            return neo;
+        }
+    }

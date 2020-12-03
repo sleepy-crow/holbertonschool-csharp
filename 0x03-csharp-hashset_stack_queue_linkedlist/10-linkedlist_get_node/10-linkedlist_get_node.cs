5ecae7115cd7e5d8da1cffc0751937e81cf95071
noah172 <jsebastian.garcia1995@gmail.com>
2020-12-03 18:40:20 -0500

    task 11

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int GetNode(LinkedList<int> myLList, int n)
+    {
+        int i = 0;
+        foreach (int x in myLList)
+        {
+            if (i == n)
+                return x;
+            i++;
+        }
+        if (i < n)
+            return 0;
+        return -1;
+
+    }
+}

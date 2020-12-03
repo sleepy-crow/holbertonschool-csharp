1236a49215f26bd29217b5f06618ec7f29947bdc
noah172 <jsebastian.garcia1995@gmail.com>
2020-12-03 16:57:56 -0500

    task 5

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedList<int> CreatePrint(int size)
+    {
+        LinkedList<int> intLinkd = new LinkedList<int>();
+
+        if (size < 0)
+            return (intLinkd);
+
+        for (int i= 0; i < size; i++)
+         {
+             Console.WriteLine(i);
+             intLinkd.AddLast(i);
+         }   
+        
+        return(intLinkd);
+    }
+}

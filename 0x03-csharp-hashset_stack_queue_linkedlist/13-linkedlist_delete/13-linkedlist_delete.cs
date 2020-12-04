475debb6ed5824312542476403859b6aeb0412e1
noah172 <jsebastian.garcia1995@gmail.com>
2020-12-03 19:13:44 -0500

    task 13

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static void Delete(LinkedList<int> myLList, int index)
+    {
+        LinkedListNode<int> currentNode = myLList.First;
+
+        if (myLList != null && index == myLList.Count - 1)
+            myLList.RemoveLast();
+
+        else if (myLList != null && index == 0)
+            myLList.RemoveFirst();
+
+        else
+        {
+            if (myLList != null && index >= 0)
+            {
+                for (int i = 0; i <= index && currentNode != null; i++)
+                {
+                    if (i == index)
+                    {
+                        myLList.Remove(currentNode);
+                        break;
+                    }
+                    currentNode = currentNode.Next;
+                }
+            }
+        }
+    }
+}

1c2f331aef6acca62f56f435d6c6a7d00e2d52f2
noah172 <jsebastian.garcia1995@gmail.com>
2020-12-14 16:25:30 -0500

    task 1

---
+++ 0x04-csharp-exceptions/1-divide_print/1-divide_print.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Int
+{
+    public static void divide(int a, int b)
+    {
+        int quot = 0;
+        try
+        {
+            quot = a / b;
+        }
+        catch (DivideByZeroException)
+        {
+            Console.WriteLine("Cannot divide by zero");
+        }
+        finally
+        {
+            Console.WriteLine($"{a} / {b} = {quot}");
+        }
+    }
+}

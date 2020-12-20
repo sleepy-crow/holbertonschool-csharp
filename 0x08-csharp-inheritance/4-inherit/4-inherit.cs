47a2fbade1f8a403165e69066225a87345e2ead0
noah172 <jsebastian.garcia1995@gmail.com>
2020-12-20 16:13:56 -0500

    added: documentation

--- 0x08-csharp-inheritance/4-inherit/4-inherit.cs
+++ 0x08-csharp-inheritance/4-inherit/4-inherit.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 
+    /// <summary> empty class Animal </summary>
     public class Animal
     {
-        // 
+        // ( ͡ಠ ͜ʖ ͡ಠ)
     }
 
+    /// <summary> also an empty class dog </summary>
     public class Dog : Animal
     {
-        // 
+        // ¯\_( ͡ಠ ͜ʖ ͡ಠ)_/¯
     }

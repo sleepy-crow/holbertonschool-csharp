bac5803559137ca2b6ef4bd7b987f3887b0c08c7
noah172 <jsebastian.garcia1995@gmail.com>
2020-12-19 20:03:25 -0500

    added: documentation

--- 0x08-csharp-inheritance/0-is/0-is.cs
+++ 0x08-csharp-inheritance/0-is/0-is.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 
+    /// <summary>This is the class Obj.</summary>
     class Obj
     {
+        /// <sumary>
+        /// method that returns True if the object is an int, otherwise return False.
+        /// </sumary>
         public static bool IsOfTypeInt(object obj)
         {
             if (obj.GetType() == typeof(int))
--- 0x08-csharp-inheritance/1-instance/1-instance.cs
+++ 0x08-csharp-inheritance/1-instance/1-instance.cs
@@ -1,8 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 
+    /// <summary>This is the class Obj.</summary>
     class Obj
     {
+        /// <sumary>
+        /// method that returns True if the object is an instance of, or if the object is an instance
+        /// of a class that inherited from, Array, otherwise return False.
+        /// </sumary>
         public static bool IsInstanceOfArray(object obj)
         {
             if (obj is Array)

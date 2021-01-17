52d9adbfb228c62dacea41b2630f5b77f6f47a18
noah172 <jsebastian.garcia1995@gmail.com>
2021-01-17 18:10:51 -0500

    task 3

---
+++ 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _3_magnitude
+{
+    /// <summary> <summary>
+    class VectorMath
+    {
+        /// <summary> <summary>
+        public static double Magnitude(double[] vector)
+        {
+            if (vector.Length < 2 || vector.Length > 3)
+                return -1;
+            
+            double r = 0;
+
+            foreach (double i in vector)
+            {
+                r += (i * i);
+            }
+            r = Math.Round(Math.Sqrt(r), 2);
+            return r;
+        }
+    }
+}

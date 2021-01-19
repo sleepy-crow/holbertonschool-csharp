4b95835f4e2ac32f4f2ea29e141a944a831fa03e
noah172 <jsebastian.garcia1995@gmail.com>
2021-01-18 20:11:02 -0500

    hotfix

--- 0x09-csharp-linear_algebra/12-dot_product/12-dot_product.cs
+++ 0x09-csharp-linear_algebra/12-dot_product/12-dot_product.cs
@@ -10,8 +10,8 @@ class VectorMath
 	/// </summary>
 	public static double DotProduct(double[] vector1, double[] vector2)
 	{
-		if (vector1.Length != vector2.Length || vector1.Length < 2 || vector1.Length > 3
-		|| vector2.Length < 2 || vector2.Length > 3)
+		if ((vector1.Length != vector2.Length) || (vector1.Length < 2 || vector1.Length > 3)
+		|| (vector2.Length < 2 || vector2.Length > 3))
             return -1;
 
         double dot = 0;

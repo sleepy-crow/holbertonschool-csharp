cb5247b7fe81f04169017c4d6c2bd61cfc19f09b
noah172 <jsebastian.garcia1995@gmail.com>
2021-01-20 17:49:44 -0500

    task 14

--- 0x09-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
+++ 0x09-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
@@ -10,22 +10,22 @@ class MatrixMath
     /// </summary>
     public static double[,] Add(double[,] matrix1, double[,] matrix2)
     {
+        double[,] oh_no = { {-1, -1}, {-1, -1} };
         if (
             (matrix1.GetLength(0) != matrix1.GetLength(1)) ||
             (matrix2.GetLength(0) != matrix2.GetLength(1)) ||
             (matrix1.GetLength(0) != matrix2.GetLength(0)) ||
-            (matrix1.GetLength(0) > 3) || (matrix1.GetLength(1) > 3)
-            (matrix1.GetLength(0) < 2) || (matrix1.GetLength(1) < 2)
-            (matrix2.GetLength(0) > 3) || (matrix2.GetLength(1) > 3)
+            (matrix1.GetLength(0) > 3) || (matrix1.GetLength(1) > 3) ||
+            (matrix1.GetLength(0) < 2) || (matrix1.GetLength(1) < 2) ||
+            (matrix2.GetLength(0) > 3) || (matrix2.GetLength(1) > 3) ||
             (matrix2.GetLength(0) < 2) || (matrix2.GetLength(1) < 2)
             )
             {
-                double[,] oh_no = { {-1, -1}, {-1, -1} };
                 return oh_no;
             }
 
             int len = matrix1.GetLength(0);
-            double[,] mew = new double[len, len]
+            double[,] mew = new double[len, len];
             for (int i = 0; i < len; i++)
             {
                 for (int j = 0; j < len; j++)

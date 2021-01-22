4c7bb9608a9ac24b658f3601c1b9348015d2fe67
noah172 <jsebastian.garcia1995@gmail.com>
2021-01-22 17:59:47 -0500

    task 14

--- 0x09-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
+++ 0x09-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
@@ -10,7 +10,7 @@ class MatrixMath
     /// </summary>
     public static double[,] Add(double[,] matrix1, double[,] matrix2)
     {
-        double[,] oh_no = { {-1, -1}, {-1, -1} };
+        double[,] oh_no = { {-1} };
         if (
             (matrix1.GetLength(0) != matrix1.GetLength(1)) ||
             (matrix2.GetLength(0) != matrix2.GetLength(1)) ||

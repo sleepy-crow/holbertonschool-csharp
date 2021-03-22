2f999e4052e8a61acc8e3f6cd622fae47f1b6d19
noah172 <jsebastian.garcia1995@gmail.com>
2021-03-22 16:06:20 -0500

    readme and task 0

---
+++ 0x0A-csharp-generics/0-queue/queue.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+
+///<summary>This is the Queue Class.</summary>
+public class Queue<T>
+{
+    ///<summary>This method returns the Queue's type.</summary>
+    public Type CheckType()
+    {
+        return (typeof(T));
+    }
+}

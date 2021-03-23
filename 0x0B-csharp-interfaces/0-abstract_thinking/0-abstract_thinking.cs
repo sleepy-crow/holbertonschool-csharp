0a94eaba99f93e36f851d137fa4cf83abad54819
noah172 <jsebastian.garcia1995@gmail.com>
2021-03-23 18:20:22 -0500

    task 0

---
+++ 0x0B-csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/// <summary>
+/// Class base
+/// </summary>
+public abstract class Base
+{
+	public string name;
+	/// <summary>
+	/// Making the method .ToString() return the property name of the class
+	/// and the type of class it is
+	/// </summary>
+	public override string ToString()
+	{
+		return name + " is a " + this.GetType();
+	}
+}

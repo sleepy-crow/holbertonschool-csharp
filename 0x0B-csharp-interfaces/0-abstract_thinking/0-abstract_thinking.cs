12ef23eee1f802046b267714b00839ceaf2e055f
noah172 <jsebastian.garcia1995@gmail.com>
2021-03-23 19:06:05 -0500

    fix the name

--- 0x0B-csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
+++ 0x0B-csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -5,13 +5,13 @@
 /// </summary>
 public abstract class Base
 {
-	public string name;
+	public string name = null;
 	/// <summary>
 	/// Making the method .ToString() return the property name of the class
 	/// and the type of class it is
 	/// </summary>
 	public override string ToString()
 	{
-		return name + " is a " + this.GetType();
+		return $"{name} is a {this.GetType()}();
 	}
 }

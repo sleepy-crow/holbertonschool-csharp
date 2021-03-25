a54f31580af36aa24ee7648a9761ffac75a5e325
noah172 <jsebastian.garcia1995@gmail.com>
2021-03-25 16:00:15 -0500

    task 0

---
+++ 0x0C-csharp-delegates_events/0-universal_health/0-universal_health.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class Player
+{
+    public string name;
+    public float maxHp;
+    public float hp;
+	public Player(string name = "Player", float maxHp = 100f)
+	{
+        this.name = name;
+        if (maxHp > 0)
+        {
+            this.hp = maxHp;
+            this.maxHp = maxHp;
+        }
+        else
+        {
+            this.hp = 100f;
+            this.maxHp = 100f;
+            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+        }
+        
+	}
+    public void PrintHealth()
+    {
+        Console.WriteLine(this.name + " has " + this.hp + "/" + this.maxHp);
+    }
+}
---
+++ 0x0C-csharp-delegates_events/README.md
@@ -0,0 +1,13 @@
+# 0x0C. C# - Delegates, Events
+
+### 0. Universal health
+
+### 1. Damage delegation
+
+### 2. Validation
+
+### 3. Modified behavior
+
+### 4. Check yourself
+
+### 5. Eventful

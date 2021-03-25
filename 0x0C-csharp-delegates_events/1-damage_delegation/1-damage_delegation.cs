afd4302a2634b78b247b5a9e8456efcb9a3f8426
noah172 <jsebastian.garcia1995@gmail.com>
2021-03-25 18:08:57 -0500

    task 1

---
+++ 0x0C-csharp-delegates_events/1-damage_delegation/1-damage_delegation.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+/// <summary>
+/// Class of the player
+/// </summary>
+public class Player
+{
+    string name;
+    float maxHp;
+    float hp;
+    /// <summary>
+    /// constructor of the player
+    /// </summary>
+    /// <param name="name"> name of the play er</param>
+    /// <param name="maxHp"> maximum of health the player is going to have </param>
+	public Player(string name = "Player", float maxHp = 100f)
+	{
+        this.name = name;
+        if (maxHp > 0)
+        {
+            this.hp = maxHp;
+            this.maxHp = maxHp;
+        }
+        else
+        {
+            this.hp = 100f;
+            this.maxHp = 100f;
+            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+        }
+        
+	}
+    /// <summary>
+    /// Method tha prints how much health the player has
+    /// </summary>
+    public void PrintHealth()
+    {
+        Console.WriteLine(this.name + " has " + this.hp + " / " + this.maxHp + " health");
+    }
+    
+    /// <summary>
+    /// delegate for the damage
+    /// </summary>
+    /// <param name="damage"> damage taked </param>
+    public delegate void CalculateHealth(float damage);
+
+    /// <summary>
+    /// method that tells how much damage is taken
+    /// </summary>
+    /// <param name="damage"> damake taken </param>
+    public void TakeDamage(float damage)
+    {
+        if (damage > 0)
+        {
+            Console.WriteLine(this.name + " takes " + damage + " damage!");
+        }
+        else
+        {
+            Console.WriteLine(this.name + " takes 0 damage!");
+        }
+    }
+    /// <summary>
+    /// method that tells how much is healed the hp
+    /// </summary>
+    /// <param name="heal"> heal to the hp </param>
+    public void HealDamage(float heal)
+    {
+        if (heal > 0)
+        {
+            Console.WriteLine(this.name + " heals " + heal + " HP!");
+        }
+        else
+        {
+            Console.WriteLine(this.name + " heals 0 HP!");
+        }
+    }
+}

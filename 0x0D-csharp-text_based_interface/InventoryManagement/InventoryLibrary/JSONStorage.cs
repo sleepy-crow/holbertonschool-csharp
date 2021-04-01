5fb3857ab519912a1c086f7fcc1ac9a23d4d9704
Noah172 <jsebastian.garcia1995@gmail.com>
2021-04-01 18:28:00 -0500

    some corrections
    in this update the method "All" has been corrected, the method "New" was created and the methods "Save and Load" need to be updated

--- 0x0D-csharp-text_based_interface/InventoryManagement/InventoryLibrary/JSONStorage.cs
+++ 0x0D-csharp-text_based_interface/InventoryManagement/InventoryLibrary/JSONStorage.cs
@@ -11,9 +11,21 @@ namespace InventoryLibrary
     {
         public Dictionary<string, object> objects = new IDictionary<string, object>();
 
-        public Dictionary<string, object> All()
+        public void All()
         {
             return (this.objects);
+        }
+        public void New(BaseClass obj)
+        {
+            objects.Add(obj.GetType().Name + "." + obj.id, obj);
+        }
+        public void Save()
+        {
+            
+        }
+        public void Load()
+        {
+            
         }
     }
 }
